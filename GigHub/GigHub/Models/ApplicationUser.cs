e059ba589d1be617e2cdbb6a01b39c96644afcc5
tanuj <tnayanam@syr.edu>
2016-12-30 22:30:21 -0500

    AddedData to Genre Table

--- GigHub/GigHub/Models/ApplicationUser.cs
+++ GigHub/GigHub/Models/ApplicationUser.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GigHub.Models
 {
@@ -18,16 +18,5 @@ namespace GigHub.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+  
 }
