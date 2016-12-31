c31edf1944acf52ff8f5fb9983b2fe39c2689cf7
tanuj <tnayanam@syr.edu>
2016-12-30 23:18:05 -0500

    Gig Form Added

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; } // this will get stored in the database and 
+        // will be captured from front end
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
