3066b965cdee6ba1901e1a346eec03508eb7764f
tanuj <tnayanam@syr.edu>
2016-12-30 23:54:22 -0500

    Saving Gig to DB

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,25 +1,48 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 using System.Linq;
+using System;
+using System.Globalization;
 using System.Web.Mvc;
 namespace GigHub.Controllers
 {
-	public class GigsController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
 
-		public GigsController ()
-		{
-			_context = new ApplicationDbContext();
-		}
+        public GigsController ()
+        {
+            _context = new ApplicationDbContext();
+        }
 
-		public ActionResult Create()
-		{
-			var viewModel = new GigFormViewModel
-			{
-				Genres = _context.Genres.ToList()
-			};
-			return View(viewModel);
-		}
-	}
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var gig = new Gig
+            {
+                ArtistId = userId,
+                DateTime = viewModel.DateTime,
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
