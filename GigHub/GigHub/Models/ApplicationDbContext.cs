5d2463a8d89b3463b21364df64babbb800e0b8a0
tanuj <tnayanam@syr.edu>
2017-01-16 04:03:47 +0600

    Add Attendance Model

--- GigHub/GigHub/Models/ApplicationDbContext.cs
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -15,6 +16,31 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            //modelBuilder.Entity<ApplicationUser>()
+            //    .HasMany(u => u.Followers)
+            //    .WithRequired(f => f.Followee)
+            //    .WillCascadeOnDelete(false);
+
+            //modelBuilder.Entity<ApplicationUser>()
+            //    .HasMany(u => u.Followees)
+            //    .WithRequired(f => f.Follower)
+            //    .WillCascadeOnDelete(false);
+
+            //modelBuilder.Entity<UserNotification>()
+            //    .HasRequired(n => n.User)
+            //    .WithMany(u => u.UserNotifications)
+            //    .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
