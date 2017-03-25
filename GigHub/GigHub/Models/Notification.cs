b1900285abb01b59e7a9787ac0ea80e843b5b5a8
tanuj <tnayanam@syr.edu>
2017-03-25 17:10:30 -0400

    Notification and notificationtype class

---
+++ GigHub/GigHub/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        //is Nullable
+        public DateTime? OrigninalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+        
+        [Required]
+        public Gig Gig { get; set; }
+
+    }
+}
---
+++ GigHub/GigHub/Models/NotificationType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GigHub.Models
+{
+    public enum NotificationType
+    {
+
+        GigCanceled = 1,
+        GigUpdated = 2,
+        GigCreated = 3
+    }
+}
